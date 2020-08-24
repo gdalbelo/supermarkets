d0e55190b81a1d890c1c8b75870207fb7b6628f6
Gabriel <santosdalbelo@gmail.com>
2020-08-24 10:49:05 -0300

    add new Interface

---
+++ src/app/supermarket.ts
@@ -0,0 +1,15 @@
+export interface ISupermarket {
+    _id: string,
+    superMarketName: string,
+    superMarketMainImage: string,
+    superMarketAdditionalImages: string,
+    locationStreet: string,
+    locationNumber: string,
+    locationZip: string,
+    locationDistrict: string,
+    locationCountry: string,
+    locationCity: string,
+    locationState: string,
+    superMarketDescription: string,
+    superMarketPhone: string
+}
